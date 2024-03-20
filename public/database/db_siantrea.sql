b15c1d967fdab82438447fca91d8d67e7c15f0e0
arvandza <arvandza@webmail.umm.ac.id>
2024-03-20 09:33:31 +0700

    update the sql

--- public/database/db_siantrea.sql
+++ public/database/db_siantrea.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 14, 2024 at 10:04 AM
+-- Generation Time: Mar 20, 2024 at 03:32 AM
 -- Server version: 10.11.6-MariaDB
 -- PHP Version: 8.2.4
 
@@ -20,6 +20,27 @@ SET time_zone = "+00:00";
 --
 -- Database: `db_siantrea`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `antrean`
+--
+
+CREATE TABLE `antrean` (
+  `id` int(11) NOT NULL,
+  `dosen_id` int(11) DEFAULT NULL,
+  `tanggal` date DEFAULT NULL,
+  `keterangan` varchar(255) DEFAULT NULL,
+  `jumlah_antrean` int(11) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `antrean`
+--
+
+INSERT INTO `antrean` (`id`, `dosen_id`, `tanggal`, `keterangan`, `jumlah_antrean`) VALUES
+(5, 3, '2024-03-18', NULL, 0);
 
 -- --------------------------------------------------------
 
@@ -53,19 +74,28 @@ CREATE TABLE `users` (
   `nama` varchar(255) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
   `prodi` varchar(255) DEFAULT NULL,
-  `role_id` int(11) DEFAULT NULL
+  `role_id` int(11) DEFAULT NULL,
+  `antrean_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `password`, `nama`, `email`, `prodi`, `role_id`) VALUES
-(3, 'dosen', '$2y$10$uSbVZ0QCfAvywyiGnLvFnuzdxbAU0kIEKcXOWRo0pXFnDXWrwmDOy', 'Dosen Test', 'dosen@example.com', 'Informatika', 2);
+INSERT INTO `users` (`id`, `username`, `password`, `nama`, `email`, `prodi`, `role_id`, `antrean_id`) VALUES
+(3, 'dosen', '$2y$10$uSbVZ0QCfAvywyiGnLvFnuzdxbAU0kIEKcXOWRo0pXFnDXWrwmDOy', 'Dosen Test', 'dosen@example.com', 'Informatika', 2, 5),
+(4, 'dosen2', '$2y$10$qg4EFvLr7xmNsV2XhBs.uOEDdyBj4oS8rnCBJp0M8OZUvqC3f7GD6', 'Dosen Dua', 'dosendua@example.com', 'Informatika', 2, NULL);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `antrean`
+--
+ALTER TABLE `antrean`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `dosen_id` (`dosen_id`);
 
 --
 -- Indexes for table `role`
@@ -78,11 +108,18 @@ ALTER TABLE `role`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `role_id` (`role_id`);
+  ADD KEY `role_id` (`role_id`),
+  ADD KEY `fk_antrean` (`antrean_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `antrean`
+--
+ALTER TABLE `antrean`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `role`
@@ -94,16 +131,23 @@ ALTER TABLE `role`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Constraints for dumped tables
 --
 
 --
+-- Constraints for table `antrean`
+--
+ALTER TABLE `antrean`
+  ADD CONSTRAINT `antrean_ibfk_1` FOREIGN KEY (`dosen_id`) REFERENCES `users` (`id`);
+
+--
 -- Constraints for table `users`
 --
 ALTER TABLE `users`
+  ADD CONSTRAINT `fk_antrean` FOREIGN KEY (`antrean_id`) REFERENCES `antrean` (`id`),
   ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`);
 COMMIT;
 
